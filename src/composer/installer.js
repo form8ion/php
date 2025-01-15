3c530870e4770f97d6d61a182daab46dd2b5f2be
Matt Travi <programmer@travi.org>
2025-01-14 18:04:36 -0600

    fix(dependencies): skip installation for an empty list

--- src/composer/installer.js
+++ src/composer/installer.js
@@ -1,8 +1,14 @@
 import {execa} from 'execa';
-import {info} from '@travi/cli-messages';
+import {info, warn} from '@travi/cli-messages';
 
 export default async function ({projectRoot, dependencies, type}) {
-  info(`Installing ${type} dependencies`, {level: 'secondary'});
+  if (dependencies.length) {
+    info(`Installing ${type} dependencies`, {level: 'secondary'});
 
-  await execa('composer', ['require', ...dependencies, ...'development' === type ? ['--dev'] : []], {cwd: projectRoot});
+    await execa(
+      'composer',
+      ['require', ...dependencies, ...'development' === type ? ['--dev'] : []],
+      {cwd: projectRoot}
+    );
+  } else warn(`No ${type} dependencies to install`);
 }
--- src/composer/installer.test.js
+++ src/composer/installer.test.js
@@ -8,21 +8,24 @@ import install from './installer.js';
 vi.mock('execa');
 
 describe('composer dependencies installer', () => {
+  const projectRoot = any.string();
   const dependencies = any.listOf(any.word);
 
   it('should install the provided dependencies', async () => {
-    const projectRoot = any.string();
-
     await install({projectRoot, dependencies, type: any.word});
 
     expect(execa).toHaveBeenCalledWith('composer', ['require', ...dependencies], {cwd: projectRoot});
   });
 
   it('should install the provided development dependencies', async () => {
-    const projectRoot = any.string();
-
     await install({projectRoot, dependencies, type: 'development'});
 
     expect(execa).toHaveBeenCalledWith('composer', ['require', ...dependencies, '--dev'], {cwd: projectRoot});
+  });
+
+  it('should not install dependencies when none are provided', async () => {
+    await install({projectRoot, dependencies: [], type: any.word});
+
+    expect(execa).not.toHaveBeenCalled();
   });
 });
